dd87637a377fdb542d190e7ac79eab4796991880
Pedro Ulisses <pedroupcm@gmail.com>
2023-10-31 15:02:15 -0300

    Inicio do Projeto

---
+++ ICProject/src/main/java/com/pedro/ICProject/IcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.ICProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcProjectApplication.class, args);
+	}
+
+}
