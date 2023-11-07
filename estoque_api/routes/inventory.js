4ad139a8a3f41d008067a91b1a7dacae37a496f7
Eduardo Inácio <eduardo.inacio@aluno.uece.br>
2023-11-07 19:33:34 -0300

    Foram feitas as mudanças no products.js e no inventory.js

--- estoque_api/routes/inventory.js
+++ estoque_api/routes/inventory.js
@@ -8,7 +8,37 @@ router.post('/add', async (req, res) => {
     const product = await Product.add(req.body);
     res.status(201).json(product);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(400).send(error.message);
+  }
+});
+
+// Remoção de produtos
+router.post('/remove', async (req, res) => {
+  try {
+    const product = await Product.remove(req.body);
+    res.status (201).json(product);
+  } catch (error) {
+    res.status (400).send(error.message);
+  }
+});
+
+// Procurar produto pelo nome ou tipo
+router.post('/search', async (req, res) => {
+  try {
+    const product = await Product.search(req.body);
+    res.status (201).json(product);
+  } catch (error) {
+    res.status (400).send(error.message);
+  }
+});
+
+// Listar produtos em ordem alfabética
+router.post('/sort', async (req, res) => {
+  try{
+    const product = await Product.sort(req.body);
+    res.status (201).json(product);
+  } catch (error) {
+    res.status (400).send(error.message);
   }
 });
 
