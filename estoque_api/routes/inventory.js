007880956198dd05321ef7165bece5774c4c132e
Pedro Ulisses <pedro.ulisses@azultecnologia.com>
2023-11-09 17:23:52 -0300

    Organizando comandos do banco de dados

--- estoque_api/routes/inventory.js
+++ estoque_api/routes/inventory.js
@@ -13,30 +13,30 @@ router.post('/add', async (req, res) => {
 });
 
 // Remoção de produtos
-router.post('/remove', async (req, res) => {
+router.delete('/remove', async (req, res) => {
   try {
     const product = await Product.remove(req.body);
-    res.status (201).json(product);
+    res.status (204).json(product);
   } catch (error) {
     res.status (400).send(error.message);
   }
 });
 
 // Procurar produto pelo nome ou tipo
-router.post('/search', async (req, res) => {
+router.get('/search', async (req, res) => {
   try {
     const product = await Product.search(req.body);
-    res.status (201).json(product);
+    res.status (200).json(product);
   } catch (error) {
     res.status (400).send(error.message);
   }
 });
 
 // Listar produtos em ordem alfabética
-router.post('/sort', async (req, res) => {
+router.get('/sort', async (req, res) => {
   try{
     const product = await Product.sort(req.body);
-    res.status (201).json(product);
+    res.status (200).json(product);
   } catch (error) {
     res.status (400).send(error.message);
   }
